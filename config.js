a87a886d75987dc6fa3d2013469d29c360883d02
Saidmusonda <157691090+Saidmusonda@users.noreply.github.com>
2024-02-04 14:02:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['255672076405', '남자친구', true],
-  ['255672076405', 'Musondajr', true], 
+  ['255783628814', '남자친구', true],
+  ['255783628814', 'Musondajr', true], 
   [''] 
 ] //owner number
 
 //global.pairingNumber = "" //put your bot number
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['255783628814','32460248586'] 
+global.prems = ['255783628814', '32460248586', '919398758484']
+global.allowed = ['255783628814', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
